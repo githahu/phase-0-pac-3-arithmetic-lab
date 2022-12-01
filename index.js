34a44e2cafdaa8636155a2835accffe89ce4f308
Edwin <edwin.njoroge@student.moringaschool.com>
2022-12-01 19:36:36 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 3));
+
+function subtract(a, b) {
+    return a - b;
+}
+console.log(subtract(34, 27));
+
+function multiply(a, b) {
+    return a * b;
+}
+console.log(2, 3)
+
+function divide(a, b) {
+    return a / b;
+}
+console.log(divide(23, 11))
+
+
+function increment(n) {
+    return n += 1
+}
+function decrement(n) {
+    return n -= 1
+
+}
+function makeInt(string) {
+    return parseInt(string, 10)
+}
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
